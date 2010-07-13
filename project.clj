b9bac9ffac7ab4737b683793baef5e8021aff977
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-07-13 17:07:58 +0200

    Initial version

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljas "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :repositories {"freehep-maven" "http://java.freehep.org/maven2"}
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+		 [org.freehep/jas-plotter "2.2.6"]])
